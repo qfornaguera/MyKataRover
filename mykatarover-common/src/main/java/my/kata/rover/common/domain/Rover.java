4728955c982b9e9302abbceb0275f4083a0dfdb6
qfornaguera <qfornaguera@gmail.com>
2018-02-21 20:52:15 +0100

    tests over requirement 3

--- mykatarover-common/src/main/java/my/kata/rover/common/domain/Rover.java
+++ mykatarover-common/src/main/java/my/kata/rover/common/domain/Rover.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import my.kata.rover.common.interfaces.CommunicationApi;
+import my.kata.rover.common.utils.Constants;
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class Rover implements CommunicationApi {
@@ -24,4 +26,37 @@ public class Rover implements CommunicationApi {
     public void recieveMovementPattern(char... actions) {
         setThingsToDo(actions);
     }
+
+    public void move(char command){
+        int [] vector = directionVector();
+        int translation = 0;
+        if(command == 'f'){
+            translation = 1;
+        }else  if (command == 'b'){
+            translation = -1;
+        }
+        getPlace().setX((getPlace().getX()+(vector[0]*translation)) % Constants.MARS_SIZE);
+        getPlace().setY((getPlace().getY()+(vector[1]*translation)) % Constants.MARS_SIZE);
+
+    }
+
+    private int[] directionVector(){
+        switch(getDirection()){
+            case N:
+                return new int[]{0, 1};
+
+            case E:
+                return new int[]{1, 0};
+
+            case S:
+                return new int[]{0, -1};
+
+            case W:
+                return new int[]{-1, 0};
+
+            default:
+                return new int[]{0,0};
+        }
+    }
+
 }
--- mykatarover-common/src/test/java/my/kata/rover/common/test/TestRover.java
+++ mykatarover-common/src/test/java/my/kata/rover/common/test/TestRover.java
@@ -2,6 +2,7 @@ package my.kata.rover.common.test;
 
 import lombok.Getter;
 import lombok.Setter;
+import my.kata.rover.common.domain.Place;
 import my.kata.rover.common.domain.Rover;
 import my.kata.rover.common.utils.Landing;
 import org.junit.Assert;
@@ -31,5 +32,22 @@ public class TestRover {
     @Test
     public void roverRecieveCommands(){
         getRover().recieveMovementPattern('f','r','b','l','f');
+    }
+
+    @Test
+    public void roverGoesBackwards(){
+        roverInitialPoint();
+        Place roverInitPlace = new Place(getRover().getPlace().getX(),getRover().getPlace().getY());
+        getRover().move('b');
+        Assert.assertTrue(roverInitPlace.getX() != getRover().getPlace().getX() || roverInitPlace.getY() != getRover().getPlace().getY());
+    }
+
+    @Test
+    public void roverGoesForward(){
+        roverInitialPoint();
+        Place roverInitPlace = new Place(getRover().getPlace().getX(),getRover().getPlace().getY());
+        getRover().move('f');
+        Assert.assertTrue(roverInitPlace.getX() != getRover().getPlace().getX() || roverInitPlace.getY() != getRover().getPlace().getY());
+
     }
 }
